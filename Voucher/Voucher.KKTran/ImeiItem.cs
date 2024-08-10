cda1f64de17b70c1af5da1df69791b1907e863e2
tuananhnl <tuananhnl@gmail.com>
2024-08-11 00:53:07 +0700

    up 2024-08-10

---
+++ Voucher/Voucher.KKTran/ImeiItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voucher.KKTran
+{
+    public class ImeiItem
+    {
+        public string ma_imei { get; set; }
+        public string ma_vt { get; set; }
+        public string ghi_chu { get; set; }
+        public string ma_kho { get; set; }
+
+    }
+}
---
+++ Voucher/Voucher.KKTran/VoucherItem.cs
@@ -0,0 +1,17 @@
+﻿using Genbyte.Component.Voucher;
+
+namespace Voucher.KKTran
+{
+    public class VoucherItem : VoucherEntity
+    {
+        public string dien_giai { get; set; }
+        public string ong_ba { get; set; }
+        public string ma_kh { get; set; }
+        public string ma_gd { get; set; }
+        public string ma_nt { get; set; }
+        public decimal ty_gia { get; set; }
+        public string loai_ct { get; set; }
+        public decimal t_so_luong { get; set; }
+        public DateTime? ngay_lct { get; set; }
+    }
+}
