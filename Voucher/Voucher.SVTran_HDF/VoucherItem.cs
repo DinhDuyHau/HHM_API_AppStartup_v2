639e33312b698a383f6412d8bbe2ffbf6c605757
duyhau <103493705+DinhDuyHau@users.noreply.github.com>
2024-09-05 23:20:33 +0700

    Update VoucherItem.cs

--- Voucher/Voucher.SVTran_HDF/VoucherItem.cs
+++ Voucher/Voucher.SVTran_HDF/VoucherItem.cs
@@ -1,4 +1,4 @@
-﻿using Genbyte.Component.Voucher;
+using Genbyte.Component.Voucher;
 
 namespace Voucher.SVTran_HDF
 {
@@ -11,5 +11,6 @@ namespace Voucher.SVTran_HDF
         public decimal tien_dat_coc { get; set; }
         public decimal t_tien_tnk { get; set; }
         public string ma_nvvc { get; set; }
+        public DateTime? fdate2 { get; set; }
     }
 }
