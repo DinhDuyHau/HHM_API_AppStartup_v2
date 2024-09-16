0c456ef9c2d0594eae6287b1f89fc08d61727820
DinhDuyHau <duyhau2khn@gmail.com>
2024-09-16 10:10:52 +0700

    Update

---
+++ Report/Report.RptSaleReturnByImei/Service.cs
@@ -0,0 +1,294 @@
+﻿using Genbyte.Base.CoreLib;
+using Genbyte.Component.Report;
+using Genbyte.Component.Report.Model;
+using Genbyte.Sys.AppAuth;
+using Genbyte.Sys.Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+
+namespace Report.RptSaleReturnByImei
+{
+    public class Service : IComponentService
+    {
+        public IMemoryCache MemoryCache { get; set; }
+
+        public IConfiguration Configuration { get; set; }
+
+        public string controller { get; set; } = "rptSaleReturnByImei";
+
+        private const int M_TABLE_VIEW_INDEX = 1;
+
+        private ConnectType connectType = ConnectType.Report;
+        public CommonObjectModel Execute(Dictionary<string, object> param)
+        {
+            ParamItem obj_param = Converter.DictionaryToObject<ParamItem>(param);
+            string sql = "";
+            List<SqlParameter> list_paras = init(obj_param, out sql);
+            DataUtils data_utis = new DataUtils(MemoryCache, Configuration);        
+            CommonObjectModel raw_model = data_utis.GetDataPaging(this.controller, sql, list_paras, obj_param, M_TABLE_VIEW_INDEX, connectType);
+            return raw_model;
+        }
+
+        public CommonObjectModel GetPdfReport(string sysid, string service_url, string controllerReport, string form_id, Dictionary<string, object> param)
+        {
+            ParamItem obj_param = Converter.DictionaryToObject<ParamItem>(param);
+            string sql = "";
+            List<SqlParameter> list_paras = init(obj_param, out sql);
+            DataUtils data_utis = new DataUtils(MemoryCache, Configuration);
+            CommonObjectModel raw_model = data_utis.GetPdfReport(sysid, service_url, controller, controllerReport, form_id, sql, list_paras, connectType);
+            return raw_model;
+        }
+
+        public List<SqlParameter> init(ParamItem obj_param, out string sql)
+        {
+            // lấy cửa hàng mặc định đăng nhập
+           
+            int user_id = Startup.UserId;
+            int admin = Startup.Admin;
+            string ma_dvcs = "";
+
+            string ma_imei = "";
+            string ma_vv = "";
+            string ma_nx = "";
+            string tk_dt = "";
+            string tk_vt = "";
+            string ma_hd = "";
+            string ma_bp = "";
+            string ds_ma_gd = "";
+            string voucher_from = "";
+            string voucher_to = "";
+            string ma_lo = "";
+            string ma_td1 = "";
+            string ma_td2 = "";
+            string ma_td3 = "";
+            int maxLength = 12;
+            int loai_du_lieu = 1;
+            string ma_nh_kho = "";
+
+            sql = @"declare @voucherList varchar(512)
+                select @voucherList = 'HDF, HD3'
+                select @date_from as date_from, @date_to as date_to
+                exec rs_rptSaleReturnByImei @date_from, @date_to, @ma_vt, @ma_dv, @ma_imei, @ma_kh, @ma_kho, @ma_vv, @ma_nx, @tk_dt,
+                      @tk_vt, @ma_hd, @ma_bp, @nh_vt1, @nh_vt2, @nh_vt3, @nh_vt4, @nh_vt5, @ma_nganh, @ds_ma_gd, @voucherList, @voucher_from, @voucher_to,
+                      @ma_dvcs,@ma_cuahang,@ma_ca, @ma_lo, @ma_td1, @ma_td2, @ma_td3, @maxLength, 'a.ngay_ct, a.ma_ct, a.so_ct', @loai_du_lieu, 'v', @user_id, @admin, @ma_nh_kho";
+
+            List<SqlParameter> list_paras = new List<SqlParameter>();
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@date_from",
+                SqlDbType = SqlDbType.DateTime,
+                SqlValue = obj_param.tu_ngay
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@date_to",
+                SqlDbType = SqlDbType.DateTime,
+                SqlValue = obj_param.den_ngay
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@voucher_from",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = voucher_from
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@voucher_to",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = voucher_to
+            });
+            
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_kh",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_kh
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_kho",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_kho
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_vt",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_vt
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_dv",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_dv
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_imei",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_imei
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@tk_dt",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = tk_dt
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@tk_vt",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = tk_vt
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@nh_vt1",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.nh_vt1
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@nh_vt2",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.nh_vt2
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@nh_vt3",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.nh_vt3
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_nx",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_nx
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_vv",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_vv
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_hd",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_hd
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_bp",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_bp
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_dvcs",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_dvcs
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_lo",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_lo
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_td1",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_td1
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_td2",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_td2
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_td3",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_td3
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ds_ma_gd",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ds_ma_gd
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@maxLength",
+                SqlDbType = SqlDbType.Int,
+                SqlValue = maxLength
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@loai_du_lieu",
+                SqlDbType = SqlDbType.Int,
+                SqlValue = loai_du_lieu
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_cuahang",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_cuahang
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_ca",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_ca
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@nh_vt4",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.nh_vt4
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@nh_vt5",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.nh_vt5
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_nganh",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = obj_param.ma_nganh
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@user_id",
+                SqlDbType = SqlDbType.Int,
+                SqlValue = user_id
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@admin",
+                SqlDbType = SqlDbType.Bit,
+                SqlValue = admin
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_nh_kho",
+                SqlDbType = SqlDbType.VarChar,
+                SqlValue = ma_nh_kho
+            });
+            return list_paras;
+        }
+
+    }
+}
