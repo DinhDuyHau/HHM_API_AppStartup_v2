28d1c1991e473fb24abaffeac9d190c6246b9ac9
Nguyễn Duy Dự <nduydu66@gmail.com>
2024-10-21 11:04:56 +0700

    api phân quyền menu theo tk

---
+++ Category/MenuRight/MenuRightController.cs
@@ -0,0 +1,59 @@
+﻿using Genbyte.Sys.AppAuth;
+using Genbyte.Sys.Common;
+using Genbyte.Sys.Common.Models;
+using MenuRight.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace MenuRight
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuRightController : ControllerBase
+    {
+        private readonly Security _security;
+
+        public MenuRightController(IOptions<Security> security)
+        {
+            this._security = security.Value;
+        }
+
+        /// <summary>
+        /// Lấy menu phân quyền cho tài khoản đăng nhập 
+        /// </summary>
+        /// <returns>MenuModal</returns>
+        [HttpGet("get_menu_rights")]
+        #region get_menu_rights
+        public IActionResult GetMenuRight()
+        {
+            try
+            {
+                CommonObjectModel model = new CommonObjectModel()
+                {
+                    success = false,
+                    message = "",
+                    result = null
+                };
+
+                Service _service = new Service();
+
+                //lấy menu right
+                List<MenuModal> menus = _service.GetMenuRight().ToList();
+                if (menus != null)
+                {
+                    model.success = true;
+                    model.result = menus;
+                }
+
+                return Ok(model);
+            }
+            catch (Exception ex)
+            {
+                Logger.Insert(Startup.Unit, $"GET -- MenuRightController/GetMenuRight", ex);
+                return BadRequest(new { message = ApiReponseMessage.Error_Runtime });
+            }
+        }
+        #endregion
+    }
+}
---
+++ Category/MenuRight/Service.cs
@@ -0,0 +1,34 @@
+﻿using Genbyte.Base.CoreLib;
+using Genbyte.Sys.AppAuth;
+using MenuRight.Model;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MenuRight
+{
+    public class Service : CoreService
+    {
+        public List<MenuModal> GetMenuRight()
+        {
+            string username = Startup.UserName;
+            int admin = Startup.Admin;
+
+            string sql = "exec rs_GetMenuRights @username, @admin";
+            List<SqlParameter> paras = new List<SqlParameter>();
+            paras.Add(new SqlParameter()
+            {
+                ParameterName = $"@username",
+                SqlDbType = SqlDbType.VarChar,
+                Value = username
+            });
+            paras.Add(new SqlParameter
+            {
+                ParameterName = "@admin",
+                SqlDbType = SqlDbType.Bit,
+                SqlValue = admin
+            });
+            List<MenuModal> menus = base.ExecSql2List<MenuModal>(sql, paras, ConnectType.Accounting);
+            return menus;
+        }
+    }
+}
