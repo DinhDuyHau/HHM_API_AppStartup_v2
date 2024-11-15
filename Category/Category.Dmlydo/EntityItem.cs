402d3945889eb3e95b9763785d44c7c7e22c0203
DinhDuyHau <duyhau2khn@gmail.com>
2024-11-15 10:56:59 +0700

    create dmlydo

---
+++ Category/Category.Dmlydo/EntityItem.cs
@@ -0,0 +1,13 @@
+﻿using Genbyte.Sys.Common;
+
+namespace Category.Dmlydo
+{
+    public class EntityItem
+    {
+        [IsPrimary]
+        public string ma_lydo { get; set; }
+
+        public string noi_dung { get; set; }
+
+    }
+}
