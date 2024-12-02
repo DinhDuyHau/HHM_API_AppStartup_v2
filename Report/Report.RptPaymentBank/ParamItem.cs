f3dc5eca69e5333cb8056f88f533f5b707a1c0e5
DinhDuyHau <duyhau2khn@gmail.com>
2024-12-02 13:19:55 +0700

    Update Report.RptPaymentBank

--- Report/Report.RptPaymentBank/ParamItem.cs
+++ Report/Report.RptPaymentBank/ParamItem.cs
@@ -4,9 +4,13 @@ namespace Report.RptPaymentBank
 {
     public class ParamItem : ParamItemBase
     {
-        public DateTime ngay_ct1 { get; set; }
+        public DateTime tu_ngay { get; set; }
 
-        public DateTime ngay_ct2 { get; set; }
+        public DateTime den_ngay { get; set; }
+
+        public string so_ct1 { get; set; } = "";
+
+        public string so_ct2 { get; set; } = "";
 
         public string ma_ca { get; set; } = "";
 
@@ -16,6 +20,26 @@ namespace Report.RptPaymentBank
 
         public string ma_kh { get; set; } = "";
 
+
+        public string ma_khqttg { get; set; } = "";
+
+        public string ma_khvdt { get; set; } = "";
+
+        public bool ck_yn { get; set; } = true;
+
+        public string tk_nh { get; set; } = "";
+
+        public bool qt_yn { get; set; } = true;
+
+        public string ma_posqt { get; set; } = "";
+
+        public bool qttg_yn { get; set; } = true;
+
+        public string ma_posqttg { get; set; } = "";
+
+        public bool vdt_yn { get; set; } = true;
+
+        public bool vnpay_yn { get; set; } = true;
 
     }
 }
