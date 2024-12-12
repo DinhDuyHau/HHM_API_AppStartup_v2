a6d7aeb2836a6a98b0a390fb46c90c7b87ac5dff
trinhhuyen31397 <trinhhuyen31397@gmail.com>
2024-12-12 11:36:38 +0700

    Update BC

--- Report/Report.RptRetailPrice/ParamItem.cs
+++ Report/Report.RptRetailPrice/ParamItem.cs
@@ -9,7 +9,7 @@ namespace Report.RptRetailPrice
 {
     public class ParamItem : ParamItemBase
     {
-        public DateTime ngay_hl { get; set; }
+        public DateTime tu_ngay { get; set; }
 
         public string ma_cuahang { get; set; }
 
