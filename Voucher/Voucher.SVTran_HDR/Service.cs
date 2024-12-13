009c9a1f914f13a41164deb2a09c3d86df325fe4
Nguyễn Duy Dự <nduydu66@gmail.com>
2024-12-13 11:42:31 +0700

    fix lỗi lưu phiếu hàng hoàn tmđt

--- Voucher/Voucher.SVTran_HDR/Service.cs
+++ Voucher/Voucher.SVTran_HDR/Service.cs
@@ -397,6 +397,7 @@ namespace Voucher.SVTran_HDR
                                             imeis.AddRange(item.ma_imei.Split(",").ToList().Select(x => x.Trim()));
                                         }
                                     });
+                                    detail_list.ForEach(x => x.stt_rec_hd = APIService.DecryptForWebApp(x.stt_rec_hd, this.aes_key, this.aes_iv));
                                     item_detail.Data = new List<DetailEntity>();
                                     item_detail.Data.AddRange(detail_list);
                                 }
