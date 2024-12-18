f82f47969780ed22080605daaa4a2c021b36f8f8
Nguyễn Duy Dự <nduydu66@gmail.com>
2024-12-18 12:09:05 +0700

    bc chi tiết hoa hồng

---
+++ Report/Report.RptCommissionCategoryReport/Service.cs
@@ -0,0 +1,105 @@
+﻿using Genbyte.Component.Report;
+using Genbyte.Component.Report.Model;
+using Genbyte.Sys.AppAuth;
+using Genbyte.Sys.Common.Models;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Report.RptCommissionCategoryReport
+{
+    public class Service : IReportService
+    {
+        public IMemoryCache MemoryCache { get; set; }
+        public IConfiguration Configuration { get; set; }
+        public string controller { get; set; } = "rptCommissionCategoryReport";
+        public readonly int table_index = 1;
+
+        public CommonObjectModel Execute(Dictionary<string, object> param)
+        {
+            ParamItem obj_param = Converter.DictionaryToObject<ParamItem>(param);
+            string sql;
+            List<SqlParameter> list_paras = init(obj_param, out sql);
+            DataUtils data_utis = new DataUtils(MemoryCache, Configuration);
+            CommonObjectModel raw_model = data_utis.GetDataPaging(this.controller, sql, list_paras, obj_param, table_index, ConnectType.Accounting);
+            return raw_model;
+        }
+
+        public CommonObjectModel GetPdfReport(string sysid, string service_url, string controllerReport, string form_id, Dictionary<string, object> param)
+        {
+            ParamItem obj_param = Converter.DictionaryToObject<ParamItem>(param);
+            string sql;
+            List<SqlParameter> list_paras = init(obj_param, out sql);
+            DataUtils data_utis = new DataUtils(MemoryCache, Configuration);
+            CommonObjectModel raw_model = data_utis.GetPdfReport(sysid, service_url, controller, controllerReport, form_id, sql, list_paras);
+            return raw_model;
+        }
+
+        public Query InitExport(string controller, Dictionary<string, object> param)
+        {
+            string sql = "";
+            ParamItem obj_param = Converter.DictionaryToObject<ParamItem>(param);
+            List<SqlParameter> list_paras = init(obj_param, out sql);
+            return new Query()
+            {
+                SqlString = sql,
+                Parameters = list_paras,
+                RptTableIndex = this.table_index
+            };
+        }
+
+        public List<SqlParameter> init(ParamItem obj_param, out string sql)
+        {
+            int user_id = Startup.UserId;
+            int admin = Startup.Admin;
+
+            sql = @"select cast(@tu_ngay as smalldatetime) as date_from, cast(@den_ngay as smalldatetime) as date_to
+                exec rs_rptCommissionCategoryReport @tu_ngay, @den_ngay, @ma_hoahong, @ma_vt, @ma_imei, 'v', @user_id, @admin";
+            List<SqlParameter> list_paras = new List<SqlParameter>();
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@tu_ngay",
+                SqlDbType = SqlDbType.DateTime,
+                SqlValue = obj_param.tu_ngay
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@den_ngay",
+                SqlDbType = SqlDbType.DateTime,
+                SqlValue = obj_param.den_ngay
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_hoahong",
+                SqlDbType = SqlDbType.Char,
+                SqlValue = obj_param.ma_hoahong
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_vt",
+                SqlDbType = SqlDbType.Char,
+                SqlValue = obj_param.ma_vt
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@ma_imei",
+                SqlDbType = SqlDbType.Char,
+                SqlValue = obj_param.ma_imei
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@user_id",
+                SqlDbType = SqlDbType.Int,
+                SqlValue = user_id
+            });
+            list_paras.Add(new SqlParameter
+            {
+                ParameterName = "@admin",
+                SqlDbType = SqlDbType.Bit,
+                SqlValue = admin
+            });
+            return list_paras;
+        }
+    }
+}
