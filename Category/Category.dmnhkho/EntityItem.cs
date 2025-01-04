41563ed6c53c34ba6658ba1e4840558659b8a762
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-01-04 12:36:56 +0700

    thêm dmnhkho

---
+++ Category/Category.dmnhkho/EntityItem.cs
@@ -0,0 +1,11 @@
+﻿using Genbyte.Sys.Common;
+
+namespace Category.Dmnhkho
+{
+    public class EntityItem
+    {
+        [IsPrimary]
+        public string ma_nh { get; set; }
+        public string ten_nh { get; set; }
+    }
+}
