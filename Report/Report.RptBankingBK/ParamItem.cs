091a54f31f8b62296241f44019df8246f569f092
trinhhuyen31397 <trinhhuyen31397@gmail.com>
2025-04-25 10:47:30 +0700

    Update bc DV ngan hang va bao kim

---
+++ Report/Report.RptBankingBK/ParamItem.cs
@@ -0,0 +1,24 @@
+﻿using Genbyte.Component.Report.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report.RptBankingBK
+{
+    public class ParamItem : ParamItemBase
+    {
+        public DateTime tu_ngay { get; set; }
+
+        public DateTime den_ngay { get; set; }
+
+        public int ngan_hang { get; set; } = 1;
+
+        public string ma_cuahang { get; set; } = "";
+
+        public string ds_ma_gd { get; set; } = "";
+
+
+    }
+}
