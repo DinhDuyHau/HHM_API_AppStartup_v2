0cf616f0ff5cbc07427c73b7c365dc971f7dde6f
Nguyễn Duy Dự <nduydu66@gmail.com>
2025-05-23 17:02:18 +0700

    fix lỗi lọc hóa đơn phiếu trả quà

--- Voucher/Voucher.SVTran_BHI/Service.cs
+++ Voucher/Voucher.SVTran_BHI/Service.cs
@@ -65,6 +65,21 @@ namespace Voucher.SVTran_BHI
         private readonly string aes_key = "";
         private readonly string aes_iv = "";
 
+        public Service(IConfiguration configuration)
+        {
+            Authoriztion authoriztion = CommonService.GetAuthoriztion("SVTran_BHI");
+            VoucherRight = new AccessRight();
+            VoucherRight.AllowReadAll = authoriztion.view_yn;
+            VoucherRight.AllowRead = authoriztion.access_yn;
+            VoucherRight.AllowCreate = authoriztion.add_yn;
+            VoucherRight.AllowUpdate = authoriztion.edit_yn;
+            VoucherRight.AllowDelete = authoriztion.del_yn;
+            this._configuration = configuration;
+
+            this.aes_key = _configuration["Security:KeyAES"];
+            this.aes_iv = _configuration["Security:IVAES"];
+        }
+
         public Service(IConfiguration configuration, string sysid)
         {
             Authoriztion authoriztion = CommonService.GetAuthoriztion(sysid);
