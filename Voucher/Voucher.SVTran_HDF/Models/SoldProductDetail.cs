ee925bbd74832cabe6ac6c81582eac28639aa928
tuananhnl <tuananhnl@gmail.com>
2025-12-02 17:49:50 +0700

    HDF: giao dịch trả lại đơn COD, bổ sung check phải trả lại full hàng hóa + dịch vụ theo phiếu bán

---
+++ Voucher/Voucher.SVTran_HDF/Models/SoldProductDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Genbyte.Component.Voucher;
+
+namespace Voucher.SVTran_HDF.Models
+{
+    internal class SoldProductDetail : DetailEntity
+    {
+        public string ma_vt { get; set; }
+
+        public string ma_imei { get; set; }
+    }
+}
---
+++ Voucher/Voucher.SVTran_HDF/Models/SoldServiceDetail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Genbyte.Component.Voucher;
+
+namespace Voucher.SVTran_HDF.Models
+{
+    internal class SoldServiceDetail : ServiceDetailBase
+    {
+    }
+}
