5e5e8920ad08317eac0468b91eab2343a1e5973d
Duy Hau <duyhau2khn@gmail.com>
2025-12-02 09:37:22 +0700

    update lưu và lấy thông tin gc_td1 (phiếu thu tiền cọc)

--- Voucher/Voucher.SVTran_BHF/Models/TTDetail.cs
+++ Voucher/Voucher.SVTran_BHF/Models/TTDetail.cs
@@ -4,5 +4,8 @@ namespace Voucher.SVTran_BHF
 {
     public class TTDetail : PaidDetailBase
     {
+        public string gc_td1 { get; set; }
+        public string gc_td2 { get; set; }
+        public string gc_td3 { get; set; }
     }
 }
